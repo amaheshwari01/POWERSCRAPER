c3fd498c68676e2392631b24571a212e06950b48
amaheshwari01 <acoding2007@gmail.com>
2023-12-08 11:24:02 -0800

    new sort

--- src/lib/components/render/classlist.tsx
+++ src/lib/components/render/classlist.tsx
@@ -20,7 +20,25 @@ const ClassList = (props: ClassListProps) => {
   const sections = data.data.student.sections.filter(
     (section: any) =>
       section.terms.filter((t: any) => t.label === term).length !== 0
-  );
+  ).sort((a: any, b: any) => {
+    let periodA = a.period.split("(");
+    let periodB = b.period.split("(");
+
+    // Compare by letter first
+    if (periodA[1] < periodB[1]) {
+      return -1;
+    }
+    if (periodA[1] > periodB[1]) {
+      return 1;
+    }
+
+    // If letters are equal, compare by number
+    return parseInt(periodA[0]) - parseInt(periodB[0]);
+
+    // I have array of objects and object.period is a string like "1(A)" or "2(B)" or "3(A)" or "4(B)"  and I want ti sort it like 1(A) 2(A) 3(A) 4(A) 1(B) 2(B) 3(B) 4(B)
+
+
+  });
 
   return (
     <Accordion allowToggle>
