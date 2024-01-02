e45bff3bda77d4331d25063759dae08cf2b8052f
amaheshwari01 <acoding2007@gmail.com>
2024-01-01 16:11:24 -0800

    Update book loader source paths

--- src/lib/components/render/Loaders/BookLoader.tsx
+++ src/lib/components/render/Loaders/BookLoader.tsx
@@ -1,9 +1,9 @@
-import { useColorMode, useColorModeValue } from "@chakra-ui/react"
+import { useColorModeValue } from "@chakra-ui/react"
 export default function BookLoader() {
   return (
     // <Box maxW="500px" maxH="300px">
 
-    <iframe scrolling="no" allowTransparency={true} height={"300px"} src={useColorModeValue("src/lib/components/render/Loaders/book.html", "src/lib/components/render/Loaders/bookdark.html")}></iframe>
+    <iframe scrolling="no" allowTransparency={true} height={"300px"} src={useColorModeValue("public/assets/loaders/book.html", "public/assets/loaders/bookdark.html")}></iframe>
 
     // </Box>
   )
