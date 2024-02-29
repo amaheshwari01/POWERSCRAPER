bc245902932c38b3b4f9cca8c630ae030cbfbb32
amaheshwari01 <acoding2007@gmail.com>
2024-02-29 12:39:05 -0800

    better width stuff

--- src/lib/components/render/Dashboard.tsx
+++ src/lib/components/render/Dashboard.tsx
@@ -1,18 +1,77 @@
-import { Button, Stack } from "@chakra-ui/react";
-import Schedule from "./schedule";
-import RecentAssignments from "./RecentAssignments";
+import React, { useEffect, useRef, useState } from 'react';
+import { Box, Button, Stack, Wrap, WrapItem } from '@chakra-ui/react';
+import Schedule from './schedule';
+import RecentAssignments from './RecentAssignments';
+import { useMeasure } from 'react-use';
 
 export default function Dashboard() {
-    return (
-        <div>
-            <Stack px={4} direction='row' spacing={4}>
+    const [ref, { x, y, width, height, top, right, bottom, left }] = useMeasure();
+    const [isWrapped, setIsWrapped] = useState(false);
+    useEffect(() => {
+        // console.log('width', width);
 
+        if (width < 830) {
+            setIsWrapped(true)
+        }
+        else {
+            setIsWrapped(false)
+        }
+    }, [width]);
+
+    // useEffect(() => {
+    //     const checkWrap = () => {
+    //         const wrapWidth = wrapRef.current.offsetWidth;
+    //         let childrenWidth = 0;
+
+    //         wrapRef.current.children.forEach((child) => {
+    //             childrenWidth += child.offsetWidth;
+    //         });
+
+    //         if (childrenWidth > wrapWidth) {
+    //             console.log('Wrapping has occurred');
+    //         } else {
+    //             console.log('Wrapping has not occurred');
+    //         }
+    //     };
+
+    //     window.addEventListener('resize', checkWrap);
+    //     checkWrap();
+
+    //     return () => window.removeEventListener('resize', checkWrap);
+    // }, []);
+
+
+
+    return (
+        <div ref={ref}>
+            <Stack px={4} direction='row' spacing={4}>
                 <Button as="a" href="/grades" colorScheme={"purple"}>Grades</Button>
                 <Button as="a" href="/moodle" colorScheme={"orange"}>Moodle</Button>
             </Stack>
-            <Schedule />
-            <RecentAssignments />
-
+            {isWrapped ? (
+                <Stack spacing={4}>
+                    {Widgets.map((widget, index) => (
+                        <Box key={index}>
+                            {widget}
+                        </Box>
+                    ))}
+                </Stack>
+            ) : (
+                <Wrap >
+                    {Widgets.map((widget, index) => (
+                        <WrapItem key={index}>
+                            {widget}
+                        </WrapItem>
+                    ))}
+                </Wrap>
+            )}
         </div>
     );
 }
+
+const Widgets = [
+    <Schedule />,
+    <Box as="a" href="/grades">
+        <RecentAssignments />
+    </Box>
+];
