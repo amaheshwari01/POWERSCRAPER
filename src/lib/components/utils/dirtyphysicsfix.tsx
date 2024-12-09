ef143631140738210b6516889213af7a18184ed6
amaheshwari01 <68670569+amaheshwari01@users.noreply.github.com>
2024-12-08 16:22:58 -0800

    moredirtyphyiscsfix

--- src/lib/components/utils/dirtyphysicsfix.tsx
+++ src/lib/components/utils/dirtyphysicsfix.tsx
@@ -5,14 +5,32 @@ export default function dirtyFix(data: any) {
     for (section in updatedData.data.student.sections) {
         // console.log(section)
         section = updatedData.data.student.sections[section];
-        if (section.name === "AP Physics 1" && section.teacherLastName === "Naumann") {
+        if (section.name === "AP Physics C: Mechanics" && section.teacherLastName === "Naumann") {
             // console.log("found section")
             let assignment: any;
             for (assignment in section.assignments) {
                 assignment = section.assignments[assignment];
-                if (assignment.title === "Momentum extra practice" && assignment.pointsPossible !== 10.8) {
-                    assignment.pointsEarned *= 0.3;
-                    assignment.pointsPossible *= 0.3;
+                if (assignment.title === "Unit 4 test" && assignment.pointsPossible !== 40.5) {
+                    assignment.pointsEarned *= 0.9;
+                    assignment.pointsPossible *= 0.9;
+                    assignment.pointsEarned = Math.round(assignment.pointsEarned * 100) / 100
+                    assignment.pointsPossible = Math.round(assignment.pointsPossible * 100) / 100
+                }
+                if (assignment.title === "Conservation of momentum lab" && assignment.pointsPossible !== 18) {
+                    assignment.pointsEarned *= 1.5;
+                    assignment.pointsPossible *= 1.5;
+                    assignment.pointsEarned = Math.round(assignment.pointsEarned * 100) / 100
+                    assignment.pointsPossible = Math.round(assignment.pointsPossible * 100) / 100
+                }
+                if (assignment.title === "Unit 3 test" && assignment.pointsPossible !== 40.5) {
+                    assignment.pointsEarned *= 0.9;
+                    assignment.pointsPossible *= 0.9;
+                    assignment.pointsEarned = Math.round(assignment.pointsEarned * 100) / 100
+                    assignment.pointsPossible = Math.round(assignment.pointsPossible * 100) / 100
+                }
+                if (assignment.title === "Coffee Filter lab" && assignment.pointsPossible !== 70) {
+                    assignment.pointsEarned *= 1.4;
+                    assignment.pointsPossible *= 1.4;
                     assignment.pointsEarned = Math.round(assignment.pointsEarned * 100) / 100
                     assignment.pointsPossible = Math.round(assignment.pointsPossible * 100) / 100
                 }
